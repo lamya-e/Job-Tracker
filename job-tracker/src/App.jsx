af7cebb990db689321ae9eca100994179f961d47
lamyae AIT SI SAID <lamya.aitsisaid10@gmail.com>
2026-04-29 01:43:05 +0200

    improve navbar and add job page

--- job-tracker/src/App.jsx
+++ job-tracker/src/App.jsx
@@ -5,12 +5,13 @@ import AllJobs from "./pages/AllJobs";
 export default function App(){
   return(
     <>
-     <nav className='flex'>
-      <div className="pr-14">Job Tracker</div>
-      <div>
-        <Link to='/'>Dashboard </Link>/
-        <Link to='/AddJob'>AddJob </Link>/  
-        <Link to='/AllJobs'>AllJobs</Link>
+     <nav className="flex m-6 justify-between">
+      <div className="text-xl pr-14 font-bold">💼Job Tracker</div>
+      <div className="flex gap-6">
+        <Link to='/' className="hover:bg-gray-300 p-1 rounded bg-gray-100"> Dashboard </Link>
+        <Link to='/AddJob' className="bg-gray-100 p-1 rounded hover:bg-gray-300 "> + Add Job </Link>
+        <Link to='/AllJobs' className="bg-gray-100 p-1 rounded hover:bg-gray-300 "> All Jobs </Link>
+        
       </div>
     </nav>
     <Routes>
--- job-tracker/src/pages/AddJob.jsx
+++ job-tracker/src/pages/AddJob.jsx
@@ -1,11 +1,23 @@
 export default function AddJob(){
     return(
         <>
-            <main className='bg-indigo-100 w-full h-screen'>
-                <h1 className="font-bold text-2xl">Add New Job</h1>
-                <h3>Track a new job application</h3>
-                <form>
-                    
+            <main className='bg-slate-100 w-full h-screen'>
+                <h1 className="font-bold text-4xl p-8 pb-2">Add New Job</h1>
+                <h3 className="text-xl pl-9 mb-5">Track a new job application</h3>
+                <form className="p-6 border bg-slate-50 mx-9 rounded-md">
+                    <label>Company Name</label>
+                    <input type="text" placeholder="google..." className="w-full p-2 border rounded my-3 " />
+                    <label >Job Position</label>
+                    <input type="text" placeholder="frontend devloper" className="w-full p-2 my-3" />
+                    <label>Status</label>
+                    <input list="status" className="w-full p-2 my-3"/>
+                    <datalist id="status">
+                        <option>pending</option>
+                        <option>accepted</option>
+                        <option>rejected</option>
+                    </datalist>
+                    <input type="submit" value='Add Application' className="bg-stone-400 p-3 mt-3 rounded-xl"/>
+
                 </form>
 
             </main>
