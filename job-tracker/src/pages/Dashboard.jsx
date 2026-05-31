3444c69ac38e958f47074237bf2087b080579b70
Rb_0 <heeyme917@gmail.com>
2026-06-01 01:07:25 +0200

    Update Dashboard.jsx

--- job-tracker/src/pages/Dashboard.jsx
+++ job-tracker/src/pages/Dashboard.jsx
@@ -67,7 +67,7 @@ export default function Dashboard(){
                 </div>
               </div>
               <span className={`px-3 py-1 rounded-full text-xs font-medium ${
-                job.status === "Accepted" ? "bg-green-100 text-green-700" : job.status === "Rejected" ? "bg-red-100 text-red-700" :"bg-yellow-100 text-yellow-700"
+                job.status === "accepted" ? "bg-green-100 text-green-700" : job.status === "rejected" ? "bg-red-100 text-red-700" :"bg-yellow-100 text-yellow-700"
               }`}>
                 {job.status}
               </span>
